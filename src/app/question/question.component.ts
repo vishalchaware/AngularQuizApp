ebe695156ec041b276df686145cbaf1ddf21bf27
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-22 08:19:24 +0530

    counter and Progress Bar

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 import { QuestionService } from '../service/question.service';
 
 @Component({
@@ -12,17 +13,19 @@ export class QuestionComponent implements OnInit {
 	public currentQuestion: number = 0;
 	public points: number = 0;
 	public counter: number = 60;
-	public questionLength: any = undefined;
+	public correctAnswer: number = 0;
+	inCorrectAnswer: number = 0;
+	interval$: any;
+	progress: string = '0';
 	constructor(private questionService: QuestionService) {}
 	ngOnInit(): void {
 		this.name = localStorage.getItem("name")!;
 		this.getAllQuestions();
+		this.startCounter();
 	}
 	getAllQuestions() {
 		this.questionService.getQuestionJson().subscribe(res => {
-			console.log(res.questions);
 			this.questionList = res.questions;
-			this.questionLength = this.questionList.length;
 		});
 	}
 	nextQuestion() {
@@ -30,5 +33,51 @@ export class QuestionComponent implements OnInit {
 	}
 	previousQuestion() {
 		this.currentQuestion--;
+	}
+	answer(currentQno: number, option: any) {
+		if(option.correct) {
+			this.points += 10;
+			this.getProgressPercent();
+			this.correctAnswer++;
+			this.currentQuestion++;
+		} else {
+			this.points = this.points - 10;
+			this.currentQuestion++;
+			this.inCorrectAnswer++;
+		}
+	}
+	startCounter(){
+		this.interval$ = interval(1000)
+			.subscribe(val => {
+				this.counter--;
+				if(this.counter === 0) {
+					this.currentQuestion++;
+					this.counter = 60;
+					this.points -= 10;
+				}
+			});
+		setTimeout(() => {
+			this.interval$.unsubscribe()
+		}, 600000)
+	}
+	stopCounter() {
+		this.interval$.unsubscribe();
+		this.counter = 0;
+	}
+	resetCounter() {
+		this.stopCounter();
+		this.counter = 60;
+		this.startCounter();
+	}
+	resetQuiz() {
+		this.resetCounter();
+		this.getAllQuestions();
+		this.points = 0;
+		this.counter = 60;
+		this.currentQuestion = 0;
+	}
+	getProgressPercent(){
+		this.progress = ((this.currentQuestion/this.questionList.length) * 100).toString();
+		return this.progress;
 	}
 }
